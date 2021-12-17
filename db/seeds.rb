093d5f01a083cbabb87ed8f4638662bedb0a2ac2
Will Brennan <willbrennan314@gmail.com>
2021-12-17 12:04:09 -0500

    Completed assignment

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,13 @@ Student.create(name: "April", grade: "10th")
 Student.create(name: "Luke", grade: "9th")
 Student.create(name: "Devon", grade: "11th")
 Student.create(name: "Sarah", grade: "10th")
+
+namespace :db do
+
+    # ...
+  
+    desc 'seed the database with some dummy data'
+    task seed: :environment do
+      require_relative './db/seeds'
+    end
+  end
